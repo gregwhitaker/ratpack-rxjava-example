9c84d6e1e6f13c31584c5832fd55f5a708d1edf1
gregwhitaker <gwhitake@gmail.com>
2017-09-12 12:55:31 -0700

    Doing work

--- src/main/java/com/github/gregwhitaker/ratpackrxjava/example/endpoints/UploadEndpoint.java
+++ src/main/java/com/github/gregwhitaker/ratpackrxjava/example/endpoints/UploadEndpoint.java
@@ -1,11 +1,21 @@
 package com.github.gregwhitaker.ratpackrxjava.example.endpoints;
 
+import com.github.gregwhitaker.ratpackrxjava.example.model.Metadata;
 import com.github.gregwhitaker.ratpackrxjava.example.services.storage.StorageService;
 import ratpack.func.Action;
 import ratpack.handling.Chain;
+import ratpack.http.TypedData;
+import ratpack.rx.RxRatpack;
+import rx.Observable;
+import rx.functions.Func1;
 
 import javax.inject.Inject;
 
+import static ratpack.jackson.Jackson.json;
+
+/**
+ * Rest endpoints that handle file uploads.
+ */
 public class UploadEndpoint implements Action<Chain> {
 
     @Inject
@@ -13,6 +23,13 @@ public class UploadEndpoint implements Action<Chain> {
 
     @Override
     public void execute(Chain chain) throws Exception {
+        chain.post("/files", ctx -> {
+            Observable<Metadata> result = RxRatpack.observe(ctx.getRequest().getBody())
+                    .flatMap((Func1<TypedData, Observable<Metadata>>) typedData -> storageService.save(typedData.getInputStream()));
 
+            RxRatpack.promiseSingle(result)
+                    .onError(ctx::error)
+                    .then(metadata -> ctx.render(json(metadata)));
+        });
     }
 }
--- src/main/java/com/github/gregwhitaker/ratpackrxjava/example/services/storage/StorageService.java
+++ src/main/java/com/github/gregwhitaker/ratpackrxjava/example/services/storage/StorageService.java
@@ -4,10 +4,11 @@ import com.github.gregwhitaker.ratpackrxjava.example.model.Metadata;
 import rx.Observable;
 
 import java.io.File;
+import java.io.InputStream;
 
 public interface StorageService {
 
-    Observable<Metadata> save();
+    Observable<Metadata> save(InputStream inputStream);
 
     Observable<File> load();
 }
